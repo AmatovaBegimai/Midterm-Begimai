cddf5566abdc1f4ed360ff3e8e53cdcdb92e7754
guest1 <2003.07.o1@gmail.com>
2021-11-22 14:49:14 +0600

    first commit

---
+++ app/src/main/java/kg/muha/midtermbegimai/MainActivity2.kt
@@ -0,0 +1,11 @@
+package kg.muha.midtermbegimai
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
